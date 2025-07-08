1e98dff7e85221436b8841e1548db55717108771
LianAnandaHaase <lian.haase@posteo.de>
2025-07-08 19:06:18 +0200

    wellerman

--- code_lian/code_lian/code_lian.ino
+++ code_lian/code_lian/code_lian.ino
@@ -34,8 +34,8 @@ struct note_hoehe_laenge {
 //globale Konstanten, hier wird der pin für den lautsprecher und und die geschwindigkeit festgelegt
 const unsigned short tone_length = 70;
 const unsigned short pin = 12;
-const unsigned short lenght_refrain = 5;
-const unsigned short lenght_strophe = 6;
+const unsigned short length_refrain = 30;
+const unsigned short length_strophe = 35;
 
 //Deklarationen von allen funktionen, dann kann man die Funktionen unten in beliebiger Reihenfolge schreiben
 void strophe ();
@@ -48,23 +48,58 @@ void playOnSpeaker (int pin, note n);
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Adjust address and dimensions if needed
 
 //array mit den lyrics des refrains
-const char* lyricsRefrain[lenght_refrain] = { "Oh", "Oh Oh", "Oh Oh Oh", "Oh Oh Oh Oh", "Oh Oh Oh Oh Oh"};
+const char* lyricsRefrain[length_refrain] = {
+  "Soon", "may", "the", "Wel", "ler","man",   // Soon may the Wellerman
+  "come", "to", "bring", "us",         // come to bring us
+  "sugar", "and", "tea", "and",        // sugar and tea and
+  "rum", "One", "day", "when",         // rum. One day when
+  "the", "ton", "guin'", "is", "done",     // the tonguin' is done
+  "We'll", "take", "our", "leave",     // We'll take our leave
+  "and", "go"                          // and go
+};
 
 //array mit den preisen an der bar
-const char* preise[lenght_strophe] = {
-    "Bier         6$",
-    "Cola         6$",
-    "Vodka Lemon  9$",
-    "Gin tonic    9$",
-    "Fanta       11$",
-    "Sprite      11$"
+const char* lyricsStrophe[length_strophe] = {
+  "There", "once", "was", "a",           // 4
+  "ship", "that", "put", "to",           // 8
+  "sea", "And", "the", "name",           // 12
+  "of", "that", "ship", "was",           // 16
+  "the", "Bil", "ly", "o'",              // 20
+  "Tea", "The", "winds", "blew",         // 24
+  "up", "her", "bow", "dipped",          // 28
+  "down", "O", "blow", "my",             // 32
+  "bul", "ly", "blow"                    // 35
 };
 
 //array von noten der strophe
-note_hoehe_laenge notesStrophe[lenght_strophe] = { {E, _4}, {G, _8}, {E, _4}, {D, _4}, {C, _4}, {B, _8} };
+note_hoehe_laenge notesStrophe[length_strophe] = {
+  {E, _8}, {F, _8}, {G, _8}, {E, _8},     // There once was a ship
+  {F, _8}, {G, _8}, {A, _8}, {G, _8},     // that put to sea
+
+  {A, _8}, {G, _8}, {F, _8}, {E, _8},     // And the name of that ship
+  {F, _8}, {E, _8}, {D, _8}, {E, _8},     // was the Billy o' Tea
+
+  {F, _8}, {G, _8}, {A, _8}, {A, _8},     // The winds blew up
+  {G, _8}, {F, _8}, {E, _8}, {D, _8},     // her bow dipped down
+
+  {E, _8}, {F, _8}, {G, _8}, {G, _8},     // O blow my bully
+  {F, _8}, {E, _8}, {D, _4}               // boys blow (huh!)
+};
 
 //array von noten des refrain
-note_hoehe_laenge notesRefrain[lenght_refrain] = { {E, _4},  {G, _4}, {E, _8},{D, _4} , {C, _4} };
+note_hoehe_laenge notesRefrain[length_refrain] = {
+  {E, _8}, {E, _8}, {F, _8}, {G, _8},    // Soon may the Weller-
+  {G, _8}, {F, _8}, {E, _8}, {D, _8},    // -man come
+
+  {E, _8}, {E, _8}, {F, _8}, {G, _8},    // To bring us sugar
+  {G, _8}, {F, _8}, {E, _8}, {D, _8},    // and tea and rum
+
+  {E, _8}, {F, _8}, {G, _8}, {A, _4},    // One day when the
+  {G, _8}, {F, _8}, {E, _8},             // tonguin's done
+
+  {D, _8}, {E, _8}, {F, _8}, {G, _4},    // We'll take our
+  {F, _8}, {E, _8}, {D, _4}              // leave and go
+};
 
 
 
@@ -80,20 +115,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-    refrain ();
     strophe ();
+    refrain ();
 }
 
 //spielt den refrain ab.
 void refrain ()
 {
-    play(notesRefrain, lyricsRefrain, lenght_refrain);
+    play(notesRefrain, lyricsRefrain, length_refrain);
 }
 
 //spielt die strophe ab.
 void strophe ()
 {
-    play(notesStrophe, preise, lenght_strophe);
+    play(notesStrophe, lyricsStrophe, length_strophe);
 }
 
 //funktion, die einen array von note abspielen kann, ihr wird das richtige array mithile eines zeigers übergeben.
